6a8244c6c22b5ed8b39ccb2ed8e68426fea76f46
jacobhsu <jacob.hsu.tw@gmail.com>
2021-08-04 10:58:15 +0800

    docs: object pick omit

---
+++ docs/zh-cn/code/object/omit.js
@@ -0,0 +1,7 @@
+var _ = require('../node_modules/lodash')
+
+var object = { 'a': 1, 'b': '2', 'c': 3 };
+ 
+console.log(_.omit(object, ['a', 'c']));
+// => { 'b': '2' }
+
---
+++ docs/zh-cn/code/object/pick.js
@@ -0,0 +1,6 @@
+var _ = require('../node_modules/lodash')
+
+var object = { 'a': 1, 'b': '2', 'c': 3 };
+ 
+console.log(_.pick(object, ['a', 'c']));
+// => { 'a': 1, 'c': 3 }
